9b1e97d3ca913b187a42bef1ca11acab2c226ad3
matheuss.fernandes <matheusfer33@hotmail.com>
2024-01-29 14:14:20 -0300

    else stmt trocado por return, melhorias feitas na funçao de escolher opçao

--- num_to_text.py
+++ num_to_text.py
@@ -1,4 +1,4 @@
-dict_unidades = {1: "um", 2: "dois", 3: "tres", 4: "quatro", 5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove",
+dict_unidades = {0: "zero", 1: "um", 2: "dois", 3: "tres", 4: "quatro", 5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove",
                  10: "dez", 11: "onze", 12: "doze", 13: "treze", 14: "quatorze", 15: "quinze", 16: "dezesseis",
                  17: "dezessete", 18: "dezoito", 19: "dezenove"}
 
@@ -11,7 +11,8 @@ dict_centenas = {1: "cento", 2: "duzentos", 3: "trezentos", 4: "quatrocentos", 5
 
 def digitar_numero():
     numero_digitado = int(input("Digite um numero de ate tres casas: "))
-    checagem(numero_digitado)
+    print(checagem(numero_digitado))
+    escolher_opcao()
 
 
 def remove_unidade(numero):
@@ -32,43 +33,44 @@ def remove_centena(numero):
 
 
 def checagem(numero_digitado):
-    if numero_digitado >= 20:
-        if len(str(numero_digitado)) == 2:
-            dezena = remove_dezena(numero_digitado)
-            unidade = remove_unidade(numero_digitado)
-            if unidade == 0:
-                print(dict_dezenas[dezena])
-            else:
-                print(f"{dict_dezenas[dezena]} e {dict_unidades[unidade]}")
+    if numero_digitado < 20:
+        return dict_unidades[numero_digitado]
 
-        if len(str(numero_digitado)) == 3:
-            centena = remove_centena(numero_digitado)
-            dezena = remove_dezena(numero_digitado)
-            unidade = remove_unidade(numero_digitado)
-            if dezena == 0 and unidade == 0:
-                print("cem")
-            elif dezena == 0:
-                print(f"{dict_centenas[centena]} e {dict_unidades[unidade]}")
-            elif unidade == 0:
-                print(f"{dict_centenas[centena]} e {dict_dezenas[dezena]}")
-            elif dezena == 1:
-                print(f"{dict_centenas[centena]} e {dict_unidades[unidade + 10]}")
-            else:
-                print(f"{dict_centenas[centena]} e {dict_dezenas[dezena]} e {dict_unidades[unidade]}")
-    else:
-        print(dict_unidades[numero_digitado])
-    escolher_opcao()
+    if len(str(numero_digitado)) == 2:
+        dezena = remove_dezena(numero_digitado)
+        unidade = remove_unidade(numero_digitado)
+        dezena_extenso = dict_dezenas[dezena]
+        if unidade != 0:
+            return f"{dezena_extenso} e {dict_unidades[unidade]} "
+        return dezena_extenso
+
+    if len(str(numero_digitado)) == 3:
+        centena = remove_centena(numero_digitado)
+        dezena = remove_dezena(numero_digitado)
+        unidade = remove_unidade(numero_digitado)
+
+        centena_extenso = dict_centenas[centena]
+        if dezena != 0 or unidade != 0:
+            if dezena == 0:
+                return f"{centena_extenso} e {dict_unidades[unidade]}"
+            if unidade == 0:
+                return f"{centena_extenso} e {dict_dezenas[dezena]}"
+            if dezena == 1:
+                return f"{centena_extenso} e {dict_unidades[unidade] + 10}"
+            return f"{centena_extenso} e {dict_dezenas[dezena]} e {dict_unidades[unidade]}"
+        if centena != 1:
+            return centena_extenso
+        return "cem"
 
 
 def escolher_opcao():
-    opcao = input("deseja digitar um novo numero ? (Y/N)").upper()
-    if opcao == "Y":
-        digitar_numero()
-    elif opcao == "N":
-        print("Encerrando programa")
-    else:
-        print("opcao invalida")
-        escolher_opcao()
+    while True:
+        opcao = input("deseja digitar um novo numero ? (Y/N)\n").upper()
+        if opcao == "Y":
+            digitar_numero()
+            break
+        if opcao == "N":
+            break
 
 
 digitar_numero()
